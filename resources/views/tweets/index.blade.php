2828b485ce2608313371377dfd86b22633ee22e6
eterein <mayu.hosoo@eterein.co.jp>
2019-11-21 13:31:24 +0900

    cssファイル追加

---
+++ resources/views/tweets/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tweet</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+</head>
+<body>
+    <header>
+        <nav class="my-navbar">
+            <a class="my-navbar-title" href="#">Tweet</a>
+            <div class="my-navbar-item">
+                <a class="item" href="#">サインイン</a>
+                |
+                <a class="item" href="#">サインアップ</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            {{-- @foreach ($posts as $post) --}}
+                <div class="tweets">
+                    <a href="#" class="tweet-name">投稿者</a>
+                    <a href="#" class="tweet-text">本文</a>
+                </div>
+            {{-- @endforeach --}}
+        </div>
+    </main>
+</body>
+</html>
