c4fa50ceb1020705dfa69f8cfbaa27f67727bc3b
eterein <mayu.hosoo@eterein.co.jp>
2019-11-25 18:13:52 +0900

    新規投稿画面のレイアウト作成

---
+++ resources/views/tweets/create.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tweet</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+</head>
+<body>
+    <header>
+        <nav class="my-navbar">
+            <a class="my-navbar-title" href="#">Tweet</a>
+            <div class="my-navbar-item">
+                <a class="item" href="#">サインイン</a>
+                |
+                <a class="item" href="#">サインアップ</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="create-post">
+            <h1>新規投稿</h1>
+            <textarea class="post-text"></textarea>
+            <div class="btn">
+                <a href="#" class="btn-item">キャンセル</a>
+                <a href="#" class="btn-item">投稿</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
