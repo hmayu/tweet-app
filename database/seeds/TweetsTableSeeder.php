e94632141f13019b564a46b79ac9a68212aaac85
eterein <mayu.hosoo@eterein.co.jp>
2019-11-25 10:54:59 +0900

    TweetsDBとUsersDBのシーダーを作成

--- database/seeds/TweetsTableSeeder.php
+++ database/seeds/TweetsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TweetsTableSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class TweetsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(5,7) as $num) {
+            DB::table('tweets')->insert([
+                'user_id' => "{$num}",
+                'text' => "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト",
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,28 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => "櫻井",
+                'email' => "sakurai@email.co.jp",
+                'password' => bcrypt('test12345'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => "相葉",
+                'email' => "aiba@email.co.jp",
+                'password' => bcrypt('test6789'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => "二宮",
+                'email' => "ninomiya@email.co.jp",
+                'password' => bcrypt('testtest1'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
     }
 }
